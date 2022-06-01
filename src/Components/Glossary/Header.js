6970420e0ce795faa23dc5a3822295fa3a3a4d16
Guy Farley <guy.e.farley@gmail.com>
2022-06-01 11:47:04 -0700

    Worked on UpdateModal

--- src/Components/Glossary/Header.js
+++ src/Components/Glossary/Header.js
@@ -1,13 +1,30 @@
 import React from 'react';
+import { withAuth0 } from '@auth0/auth0-react'
+import LoginButton from './LoginButton';
+import LogoutButton from './LogoutButton';
+import { Navbar, NavItem } from 'react-bootstrap';
+
 
 class Header extends React.Component {
 
   render() {
 
     return (
-      <p>This is the header</p>
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+
+        <NavItem>
+          {
+            this.props.auth0.isAuthenticated
+              ?
+              <LogoutButton />
+              :
+              <LoginButton />
+          }
+        </NavItem>
+      </Navbar>
+
     );
   }
 }
 
-export default Header;
+export default withAuth0(Header);
